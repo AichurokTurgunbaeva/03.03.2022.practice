6ef2035000102c7c7b3b7341a24d4f6dcaf0deb8
Notnik_KG <tasnim0800@gmail.com>
2022-03-10 12:44:42 +0600

    tuura

--- src/com/company/MyException.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class MyException extends RuntimeException {
-    public MyException() {
-    }
-    public MyException(String a) {
-        System.out.println(a);
-    }
-}
